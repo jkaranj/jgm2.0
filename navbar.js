381fa449baa1eaca75f13b2e8efc9b26d8443592
karan <jkaran1998@gmail.com>
2020-04-27 15:48:19 +0530

    navbar finally fixed

--- navbar.js
+++ navbar.js
@@ -3,55 +3,56 @@ const nav = document.querySelector(".nav");
 const bar = document.querySelector(".bar1");
 const desknav = document.querySelector(".navvan");
 const dsnav = document.querySelector("dnav");
-// const wbham = document.querySelector(".bham");
-const wwham = document.querySelector(".ham");
-ham.addEventListener("click", () => {
-    nav.classList.toggle("open");
-});
-window.addEventListener("load", () => {
-    const valueeee = ham.style.display;
-    // style = window.getComputedStyle(ham),
-    // top = style.getPropertyValue('display');
-    // console.log(valueeee);
-    console.log(ham.style.display);
-    if(ham.style.display === 'none')
-    {
-        desknav.classList = 'navvan dnav';
-        console.log("display is null");
-    }
-    else
-    {
-        desknav.classList = 'navvan nav';
-        console.log("wtf happenin");
-    }
-});
-window.addEventListener("scroll", () => {
-    if(window.pageYOffset > 100 & ham.style.display == 'none')
-    {
-        desknav.classList += ' dsnav';
-    }
-    if(window.pageYOffset <= 100 & ham.style.display == 'none')
-    {
-        desknav.classList = 'navvan dnav';
-    }
-    // else
-    // {
-    //     desknav.classList = 'navvan dnav';
-    // }
-})
+// const wwham = document.querySelector(".ham");
+
+// -------for mobile view-change color of hamberger-------
 
 window.addEventListener("scroll", () => {
     if(window.pageYOffset > 100)
     {
         bar.classList += ' bar2';
-        // wbham.classList += ' wham';
-        wwham.classList = ' wham';
-        // dsnav.style.color = rgb(192, 192, 192);
+        ham.classList = ' wham';
     }
     else
     {
         bar.classList =  'bar1';
-        // wbham.classList = 'bham fas fa-bars';
-        wwham.classList = 'ham';
+        ham.classList = 'ham';
     }
 });
+
+
+//--------mobile view opening navbar doc----------
+ham.addEventListener("click", () => {
+    nav.classList.toggle("open");
+});
+
+
+//-----------mobile to desktop view for navbar-----------
+const valueeee = getComputedStyle(ham);
+window.addEventListener("load", () => {
+    if(ham.style.display === 'none')
+    {
+        desknav.classList = 'navvan dnav';
+        console.log("display is null");
+        console.log(valueeee.display);
+    }
+    else if(valueeee.display ==="block")
+    {
+        desknav.classList = 'navvan nav';
+        console.log("display not null");
+        console.log(valueeee.display);
+    }
+});
+
+//---------desktop view navbar color change-------------
+window.addEventListener("scroll", () => {
+    if(window.pageYOffset > 100 && valueeee.display !== "block")
+    {
+        desknav.classList += ' dsnav';
+    }
+    if(window.pageYOffset <= 100 && valueeee.display!== "block")
+    {
+        desknav.classList = 'navvan dnav';
+    }
+});
+
